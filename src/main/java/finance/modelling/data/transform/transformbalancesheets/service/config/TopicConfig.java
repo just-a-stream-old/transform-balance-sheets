622060bf48a6353c4d98508eb0d6db9e975df097
keirbailey <keirinvestment@gmail.com>
2020-09-26 20:47:33 +0100

    Updated app.yml config ready for run. Also impl the correct topic config

--- src/main/java/finance/modelling/data/transform/transformbalancesheets/service/config/TopicConfig.java
+++ src/main/java/finance/modelling/data/transform/transformbalancesheets/service/config/TopicConfig.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class TopicConfig {
 
     private final String traceIdHeaderName;
-    private final String ingestEodExchangesTopic;
+    private final String ingestFmpBalanceSheetTopic;
 
     public TopicConfig(
             @Value("${spring.cloud.stream.kafka.streams.header.traceId}") String traceIdHeaderName,
-            @Value("${spring.cloud.stream.bindings.generateExchangeDataModel-in-0.destination}") String ingestEodExchangesTopic) {
+            @Value("${spring.cloud.stream.bindings.generateBalanceSheetDataModel-in-0.destination}") String ingestFmpBalanceSheetTopic) {
         this.traceIdHeaderName = traceIdHeaderName;
-        this.ingestEodExchangesTopic = ingestEodExchangesTopic;
+        this.ingestFmpBalanceSheetTopic = ingestFmpBalanceSheetTopic;
     }
 }
