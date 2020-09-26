1c6b79e3075c8dc4f52860528358a700a73be302
keirbailey <keirinvestment@gmail.com>
2020-09-26 20:26:54 +0100

    Initial project structure w/ dependencies in pom

---
+++ src/main/java/finance/modelling/data/transform/transformbalancesheets/TransformBalanceSheetsApplication.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformbalancesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformBalanceSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformBalanceSheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finance/modelling/data/transform/transformbalancesheets/TransformBalanceSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformbalancesheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformBalanceSheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
